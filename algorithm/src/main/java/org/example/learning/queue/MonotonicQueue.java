c32ff3208cc1bb6bfcf9e2442126e303b6a8e494
Liu Yuanyi <gemini_lyy@163.com>
2026-01-18 22:47:04 +0800

    add monotonic queue

---
+++ algorithm/src/main/java/org/example/learning/queue/MonotonicQueue.java
@@ -0,0 +1,54 @@
+package org.example.learning.queue;
+
+import java.util.LinkedList;
+
+public class MonotonicQueue<E extends Comparable<E>> {
+
+    LinkedList<E> queue = new LinkedList<>();
+    LinkedList<E> maxQueue = new LinkedList<>();
+    LinkedList<E> minQueue = new LinkedList<>();
+
+    public void push(E e) {
+        queue.addLast(e);
+
+        while (!maxQueue.isEmpty() && maxQueue.getLast().compareTo(e) < 0) {
+            maxQueue.pollLast();
+        }
+        maxQueue.addLast(e);
+
+        while (!minQueue.isEmpty() && minQueue.getLast().compareTo(e) > 0) {
+            minQueue.pollLast();
+        }
+        minQueue.addLast(e);
+    }
+
+    public E max() {
+        return maxQueue.getFirst();
+    }
+
+    public E min() {
+        return minQueue.getFirst();
+    }
+
+    public E pop() {
+        E deleteValue = queue.pollFirst();
+        assert deleteValue != null;
+
+        if (deleteValue.equals(maxQueue.getFirst())) {
+            maxQueue.pollFirst();
+        }
+        if (deleteValue.equals(minQueue.getFirst())) {
+            minQueue.pollFirst();
+        }
+        return deleteValue;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+}
